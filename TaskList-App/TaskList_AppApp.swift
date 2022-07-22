1485d74dcce7850079991ad035f12ced38025cdf
Mishal Udeshi <mishaludeshi@Mishals-MacBook-Pro.local>
2022-07-22 20:32:21 +0530

    first_commit

---
+++ TaskList-App/TaskList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskList_AppApp.swift
+//  TaskList-App
+//
+//  Created by Mishal Udeshi on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
